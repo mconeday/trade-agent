95f88d685e708cc20314f6d22c9174ddbfda3200
m-chao <m-chao>
2025-10-03 20:24:06 +0800

    init

---
+++ tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ceab650ccae021bc2f8fa2339595bcc8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
